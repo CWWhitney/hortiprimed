568b7b8973757a1410e7202e218357b5a28e9b86
JBSLutum <JBSLutum@users.noreply.github.com>
2023-02-01 15:51:36 +0100

    different approach, first outputs
    different approach, first outputs

--- model_1.R
+++ model_1.R
@@ -37,33 +37,42 @@ yield_reduction<-function(x, varnames)
 
 }
 production<-function(x, varnames){
-# Initialize variables
-total_yield = vv(yield, var_CV, n_years) # Total yield in kg
-disease_loss = 0.05 # Disease causes a 5% loss
-pest_loss = 0.03 # Pest infestation causes a 3% loss
-nutrient_loss = 0.02 # Nutrient deficiencies cause a 2% loss
-sunscald_loss = 0.01 # Sunscald causes a 1% loss
-blossom_end_loss = 0.02 # Blossom end rot causes a 2% loss
-watering_loss = 0.01 # Overwatering or under watering causes a 1% loss
-
-for (priming in c(FALSE, TRUE)){
-  if(priming){
-    # Calculate total yield loss
-    total_loss = (disease_loss + pest_loss + nutrient_loss + sunscald_loss + blossom_end_loss + watering_loss)*vv(priming_risk_1_reduction, var_CV, n_years)
-    # Calculate final yield
-    final_yield_priming = total_yield - (total_yield * total_loss) - (total_yield*vv(priming_yield_reduction, var_CV, n_years)
-  } else {
-    # Calculate total yield loss
-    total_loss = disease_loss + pest_loss + nutrient_loss + sunscald_loss + blossom_end_loss + watering_loss
-  # Calculate final yield
-  final_yield = total_yield - (total_yield * total_loss)
+  # Initialize variables
+  total_yield = vv(yield, var_CV, n_years) # Total yield in pounds
+  disease_loss = 0.05 # Disease causes a 5% loss
+  pest_loss = 0.03 # Pest infestation causes a 3% loss
+  nutrient_loss = 0.02 # Nutrient deficiencies cause a 2% loss
+  sunscald_loss = 0.01 # Sunscald causes a 1% loss
+  blossom_end_loss = 0.02 # Blossom end rot causes a 2% loss
+  watering_loss = 0.01 # Overwatering or under watering causes a 1% loss
+  heatwave_loss = 0.05 # Heatwave causes a 5% loss in some cases
+  priming_yield_loss = 0.1 # Priming decreases the general yield by 10% 
+  priming_loss_reduction = 0.05 #Priming reduces all losses by 50%
+  
+  # Determine if a heatwave occurs
+  heatwave = sample(c(TRUE, FALSE), 1)
+  
+  # Calculate total yield loss
+  total_loss = disease_loss + pest_loss + nutrient_loss + sunscald_loss + blossom_end_loss + watering_loss
+  
+  # Add heatwave loss if it occurs
+  if (heatwave) {
+    total_loss = total_loss + heatwave_loss
   }
-}
-# Calculate total yield loss
-total_loss = disease_loss + pest_loss + nutrient_loss + sunscald_loss + blossom_end_loss + watering_loss
+  
+  for (priming in c(TRUE, FALSE)){
+  # Reduce all losses by 10% if priming is activated
+    if (priming) {
+      total_loss_priming = total_loss - priming_loss_reduction
+      total_yield_priming = total_yield - (total_yield * priming_yield_loss)
+      # Calculate final yield
+      final_yield_priming = total_yield_priming - (total_yield_priming * total_loss_priming)
+    } else {
+      # Calculate final yield
+      final_yield = total_yield - (total_yield * total_loss)}
+  }
 
-# Calculate final yield
-final_yield = total_yield - (total_yield * total_loss)
+  
 
 return(list(final_yield, final_yield_priming))
 }
