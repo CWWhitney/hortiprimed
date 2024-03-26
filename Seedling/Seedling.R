751eb065700024a0adaa239b4c8dba251b8611e8
SHMooon <133058568+SHMooon@users.noreply.github.com>
2024-03-26 14:03:22 +0100

    add

--- Seedling/Seedling.R
+++ Seedling/Seedling.R
@@ -13,7 +13,7 @@ make_variables(estimate_read_csv("Seedling/Seedling-input (1).csv"))
 
 
 decision_function <- function(x, varnames){
-  #Setzling
+  #Setzling_yield
   Setzling <-vv(Setzling, var_CV, n_years)
   
   #Ausgabe
@@ -30,7 +30,11 @@ decision_function <- function(x, varnames){
   Salzpriming<- vv(Salzpriming, var_CV, n_years)
   Heatpriming<- vv(Heatpriming, var_CV, n_years)
   
-  Direkt_Kosten_Salzpriming<- Heizmaterial + Strom + Samen + Bewaesserung + 
+  Direkt_Kosten_ohne_Priming<- Heizmaterial + Strom + Samen + Bewaesserung + 
+    CO2 +  Cell + Vermarktungsgebuehr + sonstige_Betriebsmittel
+  Direkt_Kosten_Salzpriming_100<- Heizmaterial + Strom + Samen + Bewaesserung + 
+    CO2 +  Cell + Vermarktungsgebuehr + sonstige_Betriebsmittel + Salzpriming 
+  Direkt_Kosten_Salzpriming_200<- Heizmaterial + Strom + Samen + Bewaesserung + 
     CO2 +  Cell + Vermarktungsgebuehr + sonstige_Betriebsmittel + Salzpriming 
   Direkt_Kosten_Heatpriming<- Heizmaterial + Strom + Samen + Bewaesserung + 
     CO2 +  Cell + Vermarktungsgebuehr + sonstige_Betriebsmittel + Heatpriming
@@ -43,52 +47,86 @@ decision_function <- function(x, varnames){
   #Priming risks: How many plants are primed, time risks (how long a seedling production takes)
   
    
-  #Intervention of Priming 
+  #Intervention of Heatpriming  
   
-  for (decision_priming in c(FALSE,TRUE)){
+  for (decision_Heat in c(FALSE,TRUE)){
     
-    
-    ## Kosten für Setzlingproduction ##
-    if (decision_priming){
-      Kosten_mit_Salzpriming <-
-        Direkt_Kosten_Salzpriming + Lohnkosten
-    } else{
+    ## Kosten für Setzlingproduction ##  
+    if (decision_Heat){
       Kosten_mit_Heatpriming <- Direkt_Kosten_Heatpriming + Lohnkosten
+    } else {
+      Kosten_ohne_Priming <- Direkt_Kosten_ohne_Priming + Lohnkosten
     }
-    
-    
+   
     
     ## Total_ Gewinn_Kosten ##
-    if (decision_priming){
-      
-      net_mit_Salzpriming <- Setzling - Kosten_mit_Salzpriming
-      
-      
-      result_mit_Priming<- net_mit_Salzpriming
-    }
-    
-    if (!decision_priming){
+    if (decision_Heat){
       net_mit_Heatpriming <- Setzling - Kosten_mit_Heatpriming
       result_mit_Heatpriming<- net_mit_Heatpriming
     }
     
-  }#close implementation
+    if (!decision_Heat){
+      net_ohne_Priming <- Setzling - Kosten_ohne_Priming
+      result_ohne_Priming<- net_ohne_Priming
+    }
+    
+  }#close implementation of Heat priming 
+  
+#Intervention of Salt priming  
+  
+  for (decision_Salt_100mM in c(FALSE,TRUE)){
+    
+    ## Kosten für Setzlingproduction ##  
+  
+    if (decision_Salt_100mM) {
+      Kosten_mit_Salt_100mM <- Direkt_Kosten_Salzpriming_100 + Lohnkosten
+    } else {
+      Kosten_mit_Salt_200mM <- Direkt_Kosten_Salzpriming_200 + Lohnkosten
+    }
+    
+    ## Total_ Gewinn_Kosten ##
+
+    if (decision_Salt_100mM){
+      
+      net_mit_Salt_100mM <- Setzling - Kosten_mit_Salt_100mM
+      result_mit_Salt_100mM<- net_mit_Salt_100mM
+    }
+    
+    if (!decision_Salt_100mM){
+      net_mit_Salt_200mM <- Setzling - Kosten_mit_Salt_200mM
+      result_mit_Salt_200mM<- net_mit_Salt_200mM
+    }
+    
+    
+  }#close implementation Salt Priming  
   
   #NPV calculate
   
-  NPV_mit_Salzpriming <-
-    discount(result_mit_Priming, discount_rate, calculate_NPV = T)
-  
+  NPV_ohne_Priming <-
+    discount(result_ohne_Priming, discount_rate, calculate_NPV = T)
   NPV_mit_Heatpriming <-
     discount(result_mit_Heatpriming, discount_rate, calculate_NPV = T)
+  NPV_mit_Salt_100mM <-
+    discount(result_mit_Salt_100mM, discount_rate, calculate_NPV = T)
+  NPV_mit_Salt_200mM <-
+    discount(result_mit_Salt_200mM, discount_rate, calculate_NPV = T)
   
   
-  return(list(Salzpriming_NPV =  - NPV_mit_Salzpriming, 
+  return(list(Ohne_Priming_NPV = - NPV_ohne_Priming,
               Heatpriming_NPV = - NPV_mit_Heatpriming, 
-              NPV_bet_Priming =  NPV_mit_Salzpriming - NPV_mit_Heatpriming, 
-              Cashflow_bet_Priming =  result_mit_Priming - result_mit_Heatpriming)) 
+              Salzpriming_100mM_NPV =  - NPV_mit_Salt_100mM,
+              Salzpriming_200mM_NPV =  - NPV_mit_Salt_200mM, 
+              NPV_bet_Heat_Priming =  NPV_ohne_Priming - NPV_mit_Heatpriming,
+              NPV_bet_Salt_100mM_Priming =  NPV_ohne_Priming - NPV_mit_Salt_100mM, 
+              NPV_bet_Salt_200mM_Priming =  NPV_ohne_Priming - NPV_mit_Salt_200mM, 
+              NPV_bet_Salt_Priming =  NPV_mit_Salt_100mM - NPV_mit_Salt_200mM, 
+              Cashflow_bet_Heat_Priming =  result_ohne_Priming - result_mit_Heatpriming,
+              Cashflow_bet_Salt_100mM_Priming =  result_ohne_Priming - result_mit_Salt_100mM,
+              Cashflow_bet_Salt_200mM_Priming =  result_ohne_Priming - result_mit_Salt_200mM,
+              Cashflow_bet_Salt_Priming =  result_mit_Salt_100mM - result_mit_Salt_200mM)) 
 }
 
+print(decision_function(9))
 
 library(readr)
 input_table <- read.csv("Seedling/Seedling-input (1).csv")
@@ -105,29 +143,43 @@ mcSimulation_results1 <- mcSimulation(estimate =
 
 
 plot_distributions(mcSimulation_object = mcSimulation_results1, 
-                   vars = c("Salzpriming_NPV",
-                            "Heatpriming_NPV"),
+                   vars = c("Ohne_Priming_NPV",
+                            "Heatpriming_NPV",
+                            "Salzpriming_100mM_NPV",
+                            "Salzpriming_200mM_NPV"
+                            ),
                    method = 'smooth_simple_overlay', 
                    base_size = 10)
 
 
 decisionSupport::plot_distributions(mcSimulation_object = mcSimulation_results1, 
-                                    vars = c("Salzpriming_NPV",
-                                             "Heatpriming_NPV"),
+                                    vars = c("Ohne_Priming_NPV",
+                                             "Heatpriming_NPV",
+                                             "Salzpriming_100mM_NPV",
+                                             "Salzpriming_200mM_NPV"),
                                     method = 'boxplot')
 
 
 decisionSupport::plot_distributions(mcSimulation_object = mcSimulation_results1, 
-                                    vars = "NPV_bet_Priming",
+                                    vars = "NPV_bet_Heat_Priming",
                                     method = "smooth_simple_overlay",
-                                    old_names = "NPV_bet_Priming",
-                                    new_names = "Outcome distribution for savings")
+                                    old_names = "NPV_bet_Heat_Priming",
+                                    new_names = "Outcome distribution for saving")
 
 
 
 ###Cashflow analysis###
 plot_cashflow(mcSimulation_object = mcSimulation_results1, 
-              cashflow_var_name = "Cashflow_bet_Priming")
+              cashflow_var_name = "Cashflow_bet_Heat_Priming")
+
+plot_cashflow(mcSimulation_object = mcSimulation_results1, 
+              cashflow_var_name = "Cashflow_bet_Salt_100mM_Priming")
+
+plot_cashflow(mcSimulation_object = mcSimulation_results1, 
+              cashflow_var_name = "Cashflow_bet_Salt_200mM_Priming")
+
+plot_cashflow(mcSimulation_object = mcSimulation_results1, 
+              cashflow_var_name = "Cashflow_bet_Salt_Priming")
 
 
 ###Value of Information (VoI) analysis###
@@ -137,20 +189,22 @@ plot_cashflow(mcSimulation_object = mcSimulation_results1,
 #EVPI = (EOL : Expected Opportunity Loss)
 
 
-mcSimulation_table <- data.frame(mcSimulation_results1$x, mcSimulation_results1$y[1:3])
+mcSimulation_table <- data.frame(mcSimulation_results1$x, mcSimulation_results1$y[1:8])
 
-evpi <- multi_EVPI(mc = mcSimulation_table, first_out_var = "Priming_NPV")
+evpi <- multi_EVPI(mc = mcSimulation_table, first_out_var = "Ohne_Priming_NPV")
 
 
-plot_evpi(evpi, decision_vars = "NPV_decision_SSB_Tax")
+
+
+plot_evpi(evpi, decision_vars = "Ohne_Priming_NPV")
 
 
 #put here mcSimulation_result instead of pls_result
 compound_figure(mcSimulation_object = mcSimulation_results1, 
                 input_table = input_table, 
                 plsrResults = mcSimulation_results1, 
-                EVPIresults = evpi, decision_var_name = "NPV_decision_SSB_Tax", 
-                cashflow_var_name = "Cashflow_decision_SSB_Tax", 
+                EVPIresults = evpi, decision_var_name = "NPV_decision_Priming", 
+                cashflow_var_name = "Cashflow_decision_Priming", 
                 base_size = 5)
 
 
