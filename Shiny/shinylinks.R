24c78d5a6e99eaa7fae5a4843d7fab1f13563f92
SHMooon <133058568+SHMooon@users.noreply.github.com>
2024-07-30 10:41:35 +0200

    shiny and tomato production

---
+++ Shiny/shinylinks.R
@@ -0,0 +1,6 @@
+#shiny 
+#shiny basic
+#https://shiny.posit.co/r/getstarted/shiny-basics/lesson1/index.html
+#shiny design
+#https://shiny.posit.co/r/gallery/life-sciences/biodiversity-national-parks/
+# https://shiny.posit.co/r/gallery/
